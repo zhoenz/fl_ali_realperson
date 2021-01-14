82ecf381667fdbe46764bd5bee513e88c5eb61af
zhoenz@163.com <900123zky>
2021-01-14 19:34:33 +0800

    init

--- ios/Classes/FlAliRealpersonPlugin.m
+++ ios/Classes/FlAliRealpersonPlugin.m
@@ -1,4 +1,5 @@
 #import "FlAliRealpersonPlugin.h"
+#import <RPSDK/RPSDK.h>
 
 @implementation FlAliRealpersonPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
@@ -7,14 +8,62 @@
             binaryMessenger:[registrar messenger]];
   FlAliRealpersonPlugin* instance = [[FlAliRealpersonPlugin alloc] init];
   [registrar addMethodCallDelegate:instance channel:channel];
+    [RPSDK setup];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  if ([@"startRealPerson" isEqualToString:call.method]) {
+    UIViewController * currentController = [self currentViewController];
+    [RPSDK startWithVerifyToken:call.argument[@"token"]
+                 viewController: currentController
+                     completion:^(RPResult * _Nonnull result) {
+        // 建议接入方调用实人认证服务端接口DescribeVerifyResult，
+        // 来获取最终的认证状态，并以此为准进行业务上的判断和处理。
+        NSLog(@"实人认证结果：%@", result);
+        switch (result.state) {
+            case RPStatePass:
+                // 认证通过。
+                [channel invokeMethod:@"onRealPersonResult" arguments:@"SUCCESS"];
+                break;
+            case RPStateFail:
+                // 认证不通过。
+                 [channel invokeMethod:@"onRealPersonResult" arguments:@"FAIL"];
+                break;
+            case RPStateNotVerify:
+                // 未认证。
+                // 通常是用户主动退出或者姓名身份证号实名校验不匹配等原因导致。
+                // 具体原因可通过result.errorCode和result.message来区分（详见错误码说明）。
+                [channel invokeMethod:@"onRealPersonResult" arguments:@"CANCLE"];
+                break;
+        }
+    }];
   } else {
     result(FlutterMethodNotImplemented);
   }
 }
+
+//获取Window当前显示的ViewController
+- (UIViewController*)currentViewController{
+    //获得当前活动窗口的根视图
+    UIViewController* vc = [UIApplication sharedApplication].keyWindow.rootViewController;
+    while (1)
+    {
+        //根据不同的页面切换方式，逐步取得最上层的viewController
+        if ([vc isKindOfClass:[UITabBarController class]]) {
+            vc = ((UITabBarController*)vc).selectedViewController;
+        }
+        if ([vc isKindOfClass:[UINavigationController class]]) {
+            vc = ((UINavigationController*)vc).visibleViewController;
+        }
+        if (vc.presentedViewController) {
+            vc = vc.presentedViewController;
+        }else{
+            break;
+        }
+    }
+    return vc;
+}
+
+
 
 @end
--- ios/fl_ali_realperson.podspec
+++ ios/fl_ali_realperson.podspec
@@ -13,9 +13,14 @@ A new flutter plugin project.
   s.license          = { :file => '../LICENSE' }
   s.author           = { 'Your Company' => 'email@example.com' }
   s.source           = { :path => '.' }
+  s.source = { :git => "https://github.com/CocoaPods/Specs.git" } 
+  s.source = { :git => "https://github.com/aliyun/aliyun-specs.git" } 
   s.source_files = 'Classes/**/*'
   s.public_header_files = 'Classes/**/*.h'
   s.dependency 'Flutter'
+  s.dependency "AlicloudRPSDK", "~> 3.7.0"  
+  s.dependency "AliyunOSSiOS", "~> 2.10.8"  
+  s.dependency "AlicloudSGSecurityBody", "~> 5.4.12987657-rp"  
   s.platform = :ios, '8.0'
 
   # Flutter.framework does not contain a i386 slice. Only x86_64 simulators are supported.
